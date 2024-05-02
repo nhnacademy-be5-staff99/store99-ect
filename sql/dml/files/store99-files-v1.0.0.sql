e8a9ba78003c1f0e053d62ba063e2f76a6b763a9
yrrho2 <86106442+yrrho2@users.noreply.github.com>
2024-05-02 14:30:41 +0900

    Update store99-files-v1.0.0.sql

--- sql/dml/files/store99-files-v1.0.0.sql
+++ sql/dml/files/store99-files-v1.0.0.sql
@@ -12,5 +12,64 @@ INSERT INTO files(file_name, file_url,file_type) values
 
 
 
+-- 8번 반복하여 40개를 추가로 넣을것. 
 INSERT INTO files(file_name, file_url,file_type) values
+("defaultImage", "https://kr1-api-object-storage.nhncloudservice.com/v1/AUTH_fcb81f74e379456b8ca0e091d351a7af/store99/detail/default.png" ,"BOOK"),
+("defaultImage", "https://kr1-api-object-storage.nhncloudservice.com/v1/AUTH_fcb81f74e379456b8ca0e091d351a7af/store99/detail/default.png" ,"BOOK"),
+("defaultImage", "https://kr1-api-object-storage.nhncloudservice.com/v1/AUTH_fcb81f74e379456b8ca0e091d351a7af/store99/detail/default.png" ,"BOOK"),
+("defaultImage", "https://kr1-api-object-storage.nhncloudservice.com/v1/AUTH_fcb81f74e379456b8ca0e091d351a7af/store99/detail/default.png" ,"BOOK"),
 ("defaultImage", "https://kr1-api-object-storage.nhncloudservice.com/v1/AUTH_fcb81f74e379456b8ca0e091d351a7af/store99/detail/default.png" ,"BOOK");
+
+
+
+INSERT INTO book_images (file_id, book_id, book_image_type) value (1,1, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (2,2, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (3,3, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (4,4, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (5,5, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (6,6, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (7,7, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (8,8, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (9,9, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (10,10, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (11,11, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (12,12, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (13,13, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (14,14, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (15,15, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (16,16, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (17,17, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (18,18, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (19,19, "DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) values
+(20,20,"DETAIL"),
+(21,21,"DETAIL"),
+(22,22,"DETAIL"),
+(23,23,"DETAIL"),
+(24,24,"DETAIL"),
+(25,25,"DETAIL"),
+(26,26,"DETAIL"),
+(27,27,"DETAIL"),
+(28,28,"DETAIL"),
+(29,29,"DETAIL"),
+(30,30,"DETAIL"),
+(31,31,"DETAIL"),
+(32,32,"DETAIL"),
+(33,33,"DETAIL"),
+(34,34,"DETAIL"),
+(35,35,"DETAIL"),
+(36,36,"DETAIL"),
+(37,37,"DETAIL"),
+(38,38,"DETAIL"),
+(39,39,"DETAIL"),
+(40,40,"DETAIL"),
+(41,41,"DETAIL"),
+(42,42,"DETAIL"),
+(43,43,"DETAIL"),
+(44,44,"DETAIL"),
+(45,45,"DETAIL"),
+(46,46,"DETAIL"),
+(47,27,"DETAIL"),
+(48,48,"DETAIL"),
+(49,29,"DETAIL");
+INSERT INTO book_images (file_id, book_id, book_image_type) value (50,50, "DETAIL");
