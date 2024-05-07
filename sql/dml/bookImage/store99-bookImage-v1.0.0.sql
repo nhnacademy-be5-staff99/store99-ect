ae3e259a13f1f66ce3ee0ef4507d8f3e8fb18eae
yrrho2 <86106442+yrrho2@users.noreply.github.com>
2024-05-07 19:29:17 +0900

    Update store99-bookImage-v1.0.0.sql

--- sql/dml/bookImage/store99-bookImage-v1.0.0.sql
+++ sql/dml/bookImage/store99-bookImage-v1.0.0.sql
@@ -1,24 +1,24 @@
 
-INSERT INTO book_images (file_id, book_id, book_image_type) value (1,1, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (2,2, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (3,3, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (4,4, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (5,5, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (6,6, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (7,7, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (8,8, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (9,9, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (10,10, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (11,11, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (12,12, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (13,13, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (14,14, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (15,15, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (16,16, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (17,17, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (18,18, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (19,19, "DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) values
+INSERT INTO book_images (file_id, book_id, book_image_type) values 
+(1,1, "DETAIL"),
+(2,2, "DETAIL"),
+(3,3, "DETAIL"),
+(4,4, "DETAIL"),
+(5,5, "DETAIL"),
+(6,6, "DETAIL"),
+(7,7, "DETAIL"),
+(8,8, "DETAIL"),
+(9,9, "DETAIL"),
+(10,10,"DETAIL"),
+(11,11,"DETAIL"),
+(12,12,"DETAIL"),
+(13,13,"DETAIL"),
+(14,14,"DETAIL"),
+(15,15,"DETAIL"),
+(16,16,"DETAIL"),
+(17,17,"DETAIL"),
+(18,18,"DETAIL"),
+(19,19,"DETAIL"),
 (20,20,"DETAIL"),
 (21,21,"DETAIL"),
 (22,22,"DETAIL"),
@@ -48,5 +48,5 @@ INSERT INTO book_images (file_id, book_id, book_image_type) values
 (46,46,"DETAIL"),
 (47,47,"DETAIL"),
 (48,48,"DETAIL"),
-(49,49,"DETAIL");
-INSERT INTO book_images (file_id, book_id, book_image_type) value (50,50, "DETAIL");
+(49,49,"DETAIL"),
+(50,50,"DETAIL");
