835cb3870b10136d221b7be7434074ac16cf1120
Ahyeon Song <114563915+SongAhyeon01@users.noreply.github.com>
2024-05-08 09:01:42 +0900

    Update store99-pointHistory-v1.0.0.sql

--- sql/dml/PointHistory/store99-pointHistory-v1.0.0.sql
+++ sql/dml/PointHistory/store99-pointHistory-v1.0.0.sql
@@ -3,11 +3,11 @@ DELETE FROM point_histories;
 SET sql_safe_updates=1;
 ALTER TABLE point_histories AUTO_INCREMENT = 1;
 
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (100000, "WELCOME", now(), 1);
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (10, "PURCHASE", now(), 1);
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (-10, "REFUND", now(), 1);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (100000, "WELCOME", 2024-05-07 09:59:05, 1);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (10, "PURCHASE", 2024-05-08 13:00:05, 1);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (-10, "REFUND", 2024-05-08 15:19:05, 1);
 
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (100000, "WELCOME", now(), 2);
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (5, "REVIEW", now(), 2);
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (10, "PURCHASE", now(), 2);
-INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (-5, "CANCEL", now(), 2);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (100000, "WELCOME", 2024-05-06 08:59:05, 2);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (5, "REVIEW", 2024-05-07 09:59:05, 2);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (10, "PURCHASE", 2024-05-08 10:06:05, 2);
+INSERT INTO point_histories(point_history_value, point_history_type, created_at, user_id) VALUES (-5, "CANCEL", 2024-05-08 11:30:05, 2);
